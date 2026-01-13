ee15af1cf7fb6020585eaf1baf435aa819223212
nmmn1004 <jjj4120@gmail.com>
2026-01-13 22:06:39 +0900

    Chore: initialize spring boot project

---
+++ TripleS/2026_TRIPLES_TEAM_2_BE/src/test/java/com/team2/fa/FaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.fa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
