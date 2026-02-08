d7e1c697d8669089a7a5b4d3c7fcdb5ea756db8b
nmmn1004 <jjj4120@gmail.com>
2026-02-09 02:11:32 +0900

    Fix(swagger): Fix about swagger config

--- src/main/java/com/team2/fabackend/global/config/SwaggerConfig.java
+++ src/main/java/com/team2/fabackend/global/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -45,6 +48,10 @@ public class SwaggerConfig {
                                 """)
                         .version("v1.0.0")
                 )
+
+                .servers(List.of(
+                        new Server().url("https://dontory.duckdns.org")
+                ))
                 .addSecurityItem(securityRequirement)
                 .components(new Components()
                         .addSecuritySchemes(jwtSchemeName, securityScheme)
