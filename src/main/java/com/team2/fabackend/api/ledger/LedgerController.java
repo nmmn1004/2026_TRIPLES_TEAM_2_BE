d9dadd2387035199e0d2028e6f0eadcc36984711
nmmn1004 <jjj4120@gmail.com>
2026-02-20 08:49:22 +0900

    Bug(Ledger): No deliver userid to lodgerController

--- src/main/java/com/team2/fabackend/api/ledger/LedgerController.java
+++ src/main/java/com/team2/fabackend/api/ledger/LedgerController.java
@@ -24,36 +24,37 @@ public class LedgerController {
     @PostMapping("/add")
     @Operation(summary = "가계부 내역 저장", description = "로그인된 유저의 가계부 내역을 저장하고 관련 목표에 자동 반영")
     public ResponseEntity<Void> addLedger(
-            @PathVariable User user,
+            @AuthenticationPrincipal Long userId,
             @RequestBody LedgerRequest request) {
 
-        ledgerService.saveLedger(user.getId(), request); // 유저 ID 전달
+        ledgerService.saveLedger(userId, request); // 유저 ID 전달
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/list")
     @Operation(summary = "가계부 내역 조회", description = "현재 로그인된 유저의 모든 가계부 내역 불러오기")
     public ResponseEntity<List<Ledger>> getAllLedgers(
-            @PathVariable User user) {
+            @AuthenticationPrincipal Long userId
+    ) {
 
-        List<Ledger> ledgers = ledgerService.findAllByUserId(user.getId()); // 유저별 조회
+        List<Ledger> ledgers = ledgerService.findAllByUserId(userId); // 유저별 조회
         return ResponseEntity.ok(ledgers);
     }
 
     @PatchMapping("/{id}")
     @Operation(summary = "가계부 내역 수정", description = "특정 ID 가계부 내역 수정")
     public ResponseEntity<Void> updateLedger(
-            @PathVariable Long id,
+            @AuthenticationPrincipal Long userId,
             @RequestBody LedgerRequest request) {
 
-        ledgerService.update(id, request);
+        ledgerService.update(userId, request);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "가계부 내역 삭제", description = "특정 ID 가계부 내역 삭제")
-    public ResponseEntity<Void> deleteLedger(@PathVariable Long id) {
-        ledgerService.delete(id);
+    public ResponseEntity<Void> deleteLedger(@AuthenticationPrincipal Long userId) {
+        ledgerService.delete(userId);
         return ResponseEntity.ok().build();
     }
 }
